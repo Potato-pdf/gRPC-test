c9e8a3ded0124de0022fc5ee8c8735c63204c5e0
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-03 10:30:57 -0600

    mai..go

---
+++ ping/main.go
@@ -0,0 +1,18 @@
+package ping
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewPingServiceClient(conn)
+	
+}

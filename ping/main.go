af3154ff9d18bee9a402f4d88d29b1b00198c3ae
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-03 10:57:20 -0600

    fix: main package

--- ping/main.go
+++ ping/main.go
@@ -1,18 +1,37 @@
-package ping
+package main
 
 import (
 	"context"
 	"log"
+	"net"
+	"time"
 
+	"github.com/charizardbellako/ping/pb"
 	"google.golang.org/grpc"
 )
 
-func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewPingServiceClient(conn)
-	
+type server struct {
+	pb.UnimplementedPingPongServiceServer
 }
+
+func (s *server) SendPing(ctx context.Context, req *pb.PingRequest) (*pb.PingResponse, error) {
+	return &pb.PingResponse{
+		Message: req.Message,
+		Success: true,
+		Timestamp: time.Now().Format("2006-01-02 15:04:05"),
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPingPongServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	log.Println("server started on :50051")
+}
+
